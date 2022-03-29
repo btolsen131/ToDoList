9b25cb50d4c23d2fe88d2d22b87a6f0fddd5dc2c
Brian Olsen <btolsen131@gmail.com>
2022-03-28 21:04:28 -0400

    Username and Password verification fixed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, g 
 
 app = Flask(__name__)   
 
@@ -8,16 +8,24 @@ class User:
         self.username = username
         self.password = password
     
-    def __repr__(self) -> str:
-        pass
+    def __repr__(self):
+        return f'<User: {self.username}>'
 
 users = []
 users.append(User(id=1, username='Tony', password ='password'))
+users.append(User(id=2, username='Marty', password='passwd'))
 
 appName = "Do it already"
 
+@app.before_first_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user =user
+
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/login', methods=['GET','POST'])
 def log_in() -> 'html':
     if request.method == 'POST':
         session.pop('user_id', None)
@@ -27,23 +35,24 @@ def log_in() -> 'html':
         password = request.form['password']
 
         #check if username is in users list
-        user =[x for x in users if x.username == username][0]
-
+        user = [x for x in users if x.username == username]
+        user = user[0]
         #if the passwords match redirect to the persons profile
         if user and user.password == password:
             session['user_id'] = user.id
             return redirect(url_for('profile'))
         #send back to     
-        return redirect(url_for('login'))
-
-
+        return redirect(url_for('log_in'))
 
     return render_template('login.html',
                             the_title = appName)
 
 @app.route('/profile')
-def show_list()-> 'html':
-    pass
+def profile()-> 'html':
+    return render_template('profile.html',
+                            the_title= appName)   
+
+app.secret_key = 'todoornottodo'
 
 if __name__=='__main__':
     app.run(debug=True)
