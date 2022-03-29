9c943b2be63ead890cba8b4b0849999cea146727
Brian Olsen <btolsen131@gmail.com>
2022-03-29 17:35:03 -0400

    adding MySQL database for login info

--- DBcm.py
+++ DBcm.py
@@ -1,20 +1,29 @@
 #setting up database
+import mysql.connector
 
 dbconfig = {'host': '127.0.0.1',
-            'user':'to_do_login',
-            'password': 'password'}
+            'user':'root',
+            'password': 'fuzzbutt',
+            'database': 'do_it_already'}
 
-import mysql.connector
+
 
 conn = mysql.connector.connect(**dbconfig)
 
 cursor = conn.cursor()
 
-cursor.execute("CREATE database to_do")
+cursor.execute("drop table login;")
 
 _SQL="""create table login(
-    id integer primary key,
+    id integer primary key auto_increment,
     username varchar(20),
-    password varchar(20));"""
+    password varchar(20),
+    email varchar(30));"""
 
 #cursor.execute(_SQL)
+
+_SQLinsert= """ insert into login (username, password, email)
+                values('bto131','password','Brian@gmail.com');"""
+#cursor.execute(_SQLinsert)
+
+#conn.commit()
